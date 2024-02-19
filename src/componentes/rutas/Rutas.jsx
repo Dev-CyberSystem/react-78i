299e27cd99254798c530e876f81c4d0ab83e81ae
lourdesreyna <reynalourdes91@gmail.com>
2024-02-19 10:31:59 -0300

    carpeta de rutas

---
+++ src/componentes/rutas/Rutas.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+
+const Rutas = () => {
+  return (
+    <>
+    <Routes>
+        <Route path="/" element></Route>
+    </Routes>
+        
+      
+    </>
+  )
+}
+
+export default Rutas
