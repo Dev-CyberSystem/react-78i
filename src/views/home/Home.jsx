e4ca60ee4750c493db914ec3102bddb39028fa9f
Javier <javrodriguez2003@gmail.com>
2024-02-19 10:13:02 -0300

    tarea termianda

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,12 +1,14 @@
 
 import Carrousel from '../../components/carrousel/Carrousel'
 import Footer from '../../components/footer/Footer'
-import Navegador from '../../components/navegador/navegador'
+import Navegador from '../../components/navegador/Navegador'
+
+
 
 export const Home = () => {
   return (
     <div className='d-flex flex-column align-items-center justify-content-between'>
-    <Navegador/>
+      <Navegador/>
     <Carrousel/>
     <Footer/>
     </div>
