1c3222adabd36aec1f0656eaf975d6a233447ede
lourdesreyna <reynalourdes91@gmail.com>
2024-02-20 12:20:23 -0300

    props

---
+++ src/componentes/productos/Products.jsx
@@ -0,0 +1,13 @@
+
+
+const Products = (props) => {
+    
+  return (
+    <>
+      <h2>Tus Productos</h2>
+      <h2>Libro: {props.nombre}</h2>
+    </>
+  )
+}
+
+export default Products
--- src/views/carrito/Carrito.jsx
+++ src/views/carrito/Carrito.jsx
@@ -1,9 +1,13 @@
+import Products from "../../componentes/productos/Products"
 
 
 const Carrito = () => {
+    console.log(props.nombre)
+// las props son argumentos que se pasan desde el componente padre al hijo, atráves de atributos.
   return (
     <>
      <h2>Su Carrito</h2>
+     <Products nombre= "Los siete maridos de Evelyn Hugo"/>
     </>
   )
 }
