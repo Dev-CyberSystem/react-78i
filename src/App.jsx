0d4293853a9b3bcc82857fd7ebb8567aaf8e2894
lourdesreyna <reynalourdes91@gmail.com>
2024-02-20 11:15:06 -0300

    se agrego un componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Navegador from './componentes/navegador/Navegador';
 import Rutas from './componentes/rutas/Rutas';
+import Footer from './componentes/footer/Footer';
 
 function App() {
   return (
     <>
     <Navegador/>
     <Rutas/>
+    <Footer/>
     </>
 )
 }
---
+++ src/componentes/footer/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+  return (
+    <>
+      <p>
+        copyright-TechBook | Todos los derechos reservados
+      </p>
+    </>
+  )
+}
+
+export default Footer
