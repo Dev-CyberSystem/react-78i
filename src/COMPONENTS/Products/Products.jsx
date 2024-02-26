8b4376cd1fd81870b72b4be14c900ed621cf7d28
Enzo Castro <enzocastro110@gmail.com>
2024-02-26 08:26:44 -0300

    práctica

--- src/COMPONENTS/Products/Products.jsx
+++ src/COMPONENTS/Products/Products.jsx
@@ -1,10 +1,18 @@
+import PropTypes from 'prop-types'
+
 const Products = (props) => {
   return (
-    <>
+     <>
     <h1>Soy el producto</h1>
     <h2>Voy a llamar a un elemento: {props.nombre}, {props.numero} </h2>
     </>
     )
 }
+
+Products.propTypes = {
+    nombre: PropTypes.string.isRequired,
+    numero: PropTypes.number.isRequired,
+}
+
 
 export default Products
