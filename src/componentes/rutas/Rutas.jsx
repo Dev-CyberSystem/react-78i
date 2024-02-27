f298f248989abf6d99b8f447060f0d6f5814aa22
lourdesreyna <reynalourdes91@gmail.com>
2024-02-27 20:25:23 -0300

    pagina producto

--- src/componentes/rutas/Rutas.jsx
+++ src/componentes/rutas/Rutas.jsx
@@ -1,6 +1,6 @@
 import {Routes, Route} from "react-router-dom"
 import Home from "../../views/home/Home"
-import Contacto from "../../views/contacto/Contacto"
+import Productos from "../../views/productos/Productos"
 import Admin from "../../views/admin/Admin"
 
 
@@ -9,7 +9,7 @@ const Rutas = () => {
     <>
     <Routes>
         <Route path="/" element= {<Home/>}></Route>
-        <Route path="/contacto" element= {<Contacto/>}></Route>
+        <Route path="/productos" element= {<Productos/>}></Route>
         <Route path="/admin" element= {<Admin/>}></Route>
         <Route path="*" element= {<h1>Not Found</h1>}></Route>
     </Routes>
