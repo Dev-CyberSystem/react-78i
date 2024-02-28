05e14ed451bbf033c9f39b16442d17b513c29c9e
Enzo Castro <enzocastro110@gmail.com>
2024-02-27 21:44:41 -0300

    practica

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,33 @@
-import Body from "../../COMPONENTS/Body/Body.jsx";
-import Footer from "../../COMPONENTS/Footer/Footer.jsx";
+import React, { useState, useEffect } from 'react';
+import Body from "../../COMPONENTS/BODY/Body.jsx";
+import Footer from "../../COMPONENTS/FOOTER/Footer.jsx";
 import Products from "../../COMPONENTS/Products/Products.jsx";
+import axios from "axios";
 
 const Home = () => {
+
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      try {
+        const response = await axios.get("src/COMPONENTS/FakeApi/db.json");
+        setUsers(response.data);
+        console.log('Usuarios:', response.data);
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    };
+    obtenerDatos();
+  }, []);
+
   return (
-    <>
     <div className="container d-flex flex-column align-items-center justify-content-between">
       <Body/>
-      <Products nombre = "ENZO" numero = {912}/>
+      <Products nombre="ENZO" numero={912} />
       <Footer/>
     </div>
-    </>
   );
 }
 
 export default Home;
+
