1d79bb32c066d14efa92c4dd575165402005cb4d
Gabriela <maria.g.monteros@gmail.com>
2024-03-02 08:39:08 -0300

    repasos

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navegador from "./components/navegador/Navegador";
 import Rutas from "./components/rutas/Rutas";
 
+
 function App() {
   return (
     <>
+    
       <Navegador />
       <Rutas />
+    
+    
+    
     </>
   );
 }
