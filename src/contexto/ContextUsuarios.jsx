6a4b9025c28df6ea6231b376c925c2dfadd590f6
lourdesreyna <reynalourdes91@gmail.com>
2024-03-03 11:28:14 -0300

    tarea terminada de la clase del miercoles 28/2

--- src/contexto/ContextUsuarios.jsx
+++ src/contexto/ContextUsuarios.jsx
@@ -7,7 +7,7 @@ const ContextUsuarios = ({children}) => {
   const [usuarios, setUsuario] = useState ([])
 
    //Función asincronica lo indica el async.
-   // get
+   // get ---> trae productos.
     const getApi = async () => {
       try { 
         const response = await axios.get ("http://localhost:8000/usuarios") // con el axios get se traen los datos creados en la fakeApi (se levanto un servidor para la api con el json-server).
@@ -20,7 +20,7 @@ const ContextUsuarios = ({children}) => {
     useEffect(() => {
     getApi() //se ejecuta la llamada a la api.
     }, [])
-    
+    // los children hace referencia a los componentes que estan dentro de context usuarios en el app.
    return (
     <>
     <UsuariosContext.Provider value={{usuarios, getApi}}>
