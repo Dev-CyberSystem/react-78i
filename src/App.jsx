3fa8c32ec4b40e288036cf04d0d9be37d9598d45
lourdesreyna <reynalourdes91@gmail.com>
2024-03-03 11:18:42 -0300

    tarea de la clase pasada terminada

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Navegador from './componentes/navegador/Navegador';
 import Rutas from './componentes/rutas/Rutas';
 import Footer from './componentes/footer/Footer';
 import ContextUsuarios from './contexto/ContextUsuarios';
+//se importa el componente en donde se creo el create context. y dentro de el envolvemos los demas.
 
 function App() {
   return (
     <>
-    <ContextUsuarios>
+    <ContextUsuarios> 
     <Navegador/>
     <Rutas/>
     <Footer/>
--- src/contexto/ContextUsuarios.jsx
+++ src/contexto/ContextUsuarios.jsx
@@ -7,6 +7,7 @@ const ContextUsuarios = ({children}) => {
   const [usuarios, setUsuario] = useState ([])
 
    //Función asincronica lo indica el async.
+   // get
     const getApi = async () => {
       try { 
         const response = await axios.get ("http://localhost:8000/usuarios") // con el axios get se traen los datos creados en la fakeApi (se levanto un servidor para la api con el json-server).
