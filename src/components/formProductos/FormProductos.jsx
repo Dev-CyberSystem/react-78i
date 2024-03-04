63d6ed9cbd7dc160c635b0c498f6b51bb32aa896
Facundo Arebalo <facuar1589@gmail.com>
2024-03-04 16:33:12 -0300

    editar form

--- src/components/formProductos/FormProductos.jsx
+++ src/components/formProductos/FormProductos.jsx
@@ -4,54 +4,49 @@ import { ProductosProvider } from "../../context/ProductsContext";
 import { v4 as uuidv4 } from "uuid"
 import PropTypes  from 'prop-types';
 
-const FormProductos = ({editProducto, handleClose}) => {
-  
-const {addProductos, editarProducto} =  useContext(ProductosProvider);
+// eslint-disable-next-line react/prop-types
+const FormProductos = ({ editProducto, handleClose }) => {
+  const { addProductos, editarProducto } = useContext(ProductosProvider);
 
-  const [producto, setProductos] = useState({
+  const [producto, setProducto] = useState({
     id: editProducto ? editProducto.id : uuidv4(),
-    nombre: editProducto ?  editProducto.nombre : "",
-    precio: editProducto ?  editProducto.precio : 0,
-  })
+    nombre: editProducto ? editProducto.nombre : "xxxxxxxxxxx",
+    precio: editProducto ? editProducto.precio : 43,
+  });
 
-const handleChange  = (e) =>{
-setProductos ({
-  ...producto,
-  [e.target.name]: e.target.value
-})
-}
-
-
-  
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Evita que se recargue la pagina
-    
-    if(editProducto) {
-      editarProducto(producto);
-      handleClose()
-      } else {
-        addProductos(producto)
-    setProductos({
-      id: uuidv4(),
-      nombre:"",
-      precio: 0,
-      });
-  }; 
-    
+  const handleChange = (e) => {
+    setProducto({
+      ...producto, // recuperar los datos que tenemos actualmente en el estado.
+      [e.target.name]: e.target.value, // Actualizar el estado con el nuevo valor
+    });
   };
 
- 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita que se recargue la pagina
+
+    if (editProducto) {
+      editarProducto(producto);
+      handleClose()
+    } else {
+      addProductos(producto);
+      setProducto({
+        id: uuidv4(),
+        nombre: "",
+        precio: 0,
+      });
+    }
+  };
 
   return (
     <>
-    <h2>Formulario de productos</h2>
+      <h2>Formulario de productos</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
           <Form.Label>Nombre</Form.Label>
           <Form.Control
             type="text"
-            value={producto.nombre}
             name="nombre"
+            value={producto.nombre}
             onChange={handleChange}
             placeholder="Nombre del producto"
           />
@@ -61,30 +56,27 @@ setProductos ({
           <Form.Control
             type="number"
             value={producto.precio}
-            name="precio"
             onChange={handleChange}
+            name="precio"
             placeholder="Precio"
           />
         </Form.Group>
 
-        {
-          editProducto ? (
-            <Button type="submit" variant="success"> {" "} Editar Producto
-            {" "}</Button>
-            )  : (
-          
+        {editProducto ? (
+          <Button type="submit" variant="success">
+            {" "}
+            Editar Producto{" "}
+          </Button>
+        ) : (
           <Button type="submit"> Agregar Productos </Button>
-          
-          )
-        }
-
-
+        )}
       </Form>
     </>
   );
 };
+
 FormProductos.propTypes = {
-editarProducto  : PropTypes.object,
+  editProducto: PropTypes.object,
 };
 
 export default FormProductos;
