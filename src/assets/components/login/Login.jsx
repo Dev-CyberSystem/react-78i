3f2f04830cf1ebdb6bf35e7e89aa1bf143604481
Monica Ruiz <rumoni85@gmail.com>
2024-03-04 10:47:14 -0300

    agregando login

---
+++ src/assets/components/login/Login.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const Login = () =>{ 
+
+    const [email, setEmail] = useState("");
+    const [passwiord, setPassword]= useState("");
+
+    const handleChange = (e) => {};
+
+    const handleSubmit = (e) = {};
+}
