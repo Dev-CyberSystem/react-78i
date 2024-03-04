22ea8e1b28b9b9d28cd0eb60773dd8ff86f37fbb
facundo <facurmedina@gmail.com>
2024-03-04 10:55:57 -0300

    cambios entrante de dev

--- src/views/admin/Admin.jsx
+++ src/views/admin/Admin.jsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import FormUsuarios  from "../../componentes/formulario/FormUsuarios";
-import TablaUsuarios from "../../componentes/tabla/TablaUsuarios";
-
-const Admin = () => {
-    return (
-        <div>
-            <h1>Página Admin</h1>
-            <FormUsuarios/>
-            <TablaUsuarios/>
-        </div>
-    )
-} 
-
-export default Admin
-=======
 import { Container, Row, Col } from "react-bootstrap";
 import FormProductos from "../../components/formProductos/FormProductos";
 import TableProducts from "../../components/tablas/TableProducts";
@@ -38,4 +22,3 @@ const Admin = () => {
 };
 
 export default Admin;
->>>>>>> c3b355c910369594c1d61c5810473cc48188773f
