b373e2bc6dd86726983c4519fbdf8fb1b252e094
lourdesreyna <reynalourdes91@gmail.com>
2024-03-09 11:17:32 -0300

    crear la funcion post

--- src/componentes/login/Login.jsx
+++ src/componentes/login/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import {Form, Button}from 'react-bootstrap';
-import { useNavigate } from 'react-react-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = ({handleClose}) => {
--- src/contexto/ContextUsuarios.jsx
+++ src/contexto/ContextUsuarios.jsx
@@ -8,7 +8,7 @@ const ContextUsuarios = ({children}) => {
 
    //Función asincronica lo indica el async.
    // get ---> trae productos.
-    const getApi = async () => {
+    const getUsuario = async () => {
       try { 
         const response = await axios.get ("http://localhost:8000/usuarios") // con el axios get se traen los datos creados en la fakeApi (se levanto un servidor para la api con el json-server).
         setUsuario (response.data) // se guardan los datos del http.
@@ -18,12 +18,12 @@ const ContextUsuarios = ({children}) => {
       }
     }
     useEffect(() => {
-    getApi() //se ejecuta la llamada a la api.
+    getUsuario() //se ejecuta la llamada a la api.
     }, [])
     // los children hace referencia a los componentes que estan dentro de context usuarios en el app.
    return (
     <>
-    <UsuariosContext.Provider value={{usuarios, getApi}}>
+    <UsuariosContext.Provider value={{usuarios, getUsuario}}>
         {children}
     </UsuariosContext.Provider>
       
